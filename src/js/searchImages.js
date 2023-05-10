3d624a036df492c3418f351f50ab2edd4576dd98
kutsarvasya <vkutsar89@gmail.com>
2023-05-10 22:46:28 +0200

    Update searchImages.js

--- src/js/searchImages.js
+++ src/js/searchImages.js
@@ -1,34 +1,16 @@
 import 'regenerator-runtime/runtime';
 import axios from 'axios';
 
-// const BASE_URL = 'https://pixabay.com/api/?';
-// const params = new URLSearchParams({
-//   key: '36255755-2fbf092869753c62ec4fb113a',
-//   q: '',
-//   image_type: 'photo',
-//   orientation: 'horizontal',
-//   safesearch: 'true',
-//   per_page: '40',
-//   page: '',
-// });
-
-// export async function onSearchImages(name, pageNumber) {
-//   params.set('q', name);
-//   params.set('page', pageNumber);
-//   const url = BASE_URL + params;
-//   const res = await axios.get(url);
-//   return res.data;
-// }
-
 export default class NewsApiService {
-    constructor() {
-    this.TOTAL_PAGES = 1
-    this.PER_PAGE=40
+  constructor() {
+    this.TOTAL_PAGES = 1;
+    this.PER_PAGE = 40;
+    this.BASE_URL = 'https://pixabay.com/api/?'
+    this.totalHits = null
     this.searchQuery = '';
     this.page = 1;
   }
   async querySearchImages() {
-    const BASE_URL = 'https://pixabay.com/api/?';
     const params = new URLSearchParams({
       key: '36255755-2fbf092869753c62ec4fb113a',
       q: this.searchQuery,
@@ -38,23 +20,29 @@ export default class NewsApiService {
       per_page: this.PER_PAGE,
       page: this.page,
     });
-    const url = BASE_URL + params;
-      const res = await axios.get(url);
-      this.incrementPage()
-      this.TOTAL_PAGES = Math.ceil(res.data.totalHits/ this.PER_PAGE)
-      console.log(this.TOTAL_PAGES)
+      
+    const url = this.BASE_URL + params;
+    const res = await axios.get(url);
+      this.incrementPage();
+      this.totalHits = res.data.totalHits
+      console.log(this.totalHits );
+    this.TOTAL_PAGES = Math.ceil(this.totalHits / this.PER_PAGE);
     return res.data;
   }
-    resetPage() {
-        this.page =1
-    }
-    incrementPage() {
-         this.page += 1
-    }
-    get query() {
-        return this.searchQuery
-    }
-    set query(newQuery) {
-        this.searchQuery = newQuery
-    } 
+    
+  resetPage() {
+    this.page = 1;
+  }
+    
+  incrementPage() {
+    this.page += 1;
+  }
+    
+  get query() {
+    return this.searchQuery;
+  }
+    
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
 }
